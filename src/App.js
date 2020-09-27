c3986fe201960b8fcf201a5998bf6fd25b4dc0ac
jose arturo <arturo_pdrzbn@hotmail.com>
2020-09-26 19:00:24 -0500

    components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import {Grid} from '@material-ui/core';
+
+
+import {SearchBar, VideoDetail} from './components';
+//import {VideoList} from './components/VideoList';
+
+
+import youtube from './api/Youtube';
+
 
 class App extends React.Component{
 
+    handleSubmit= async (searchTerm) => { 
+        const response = await youtube.get('search', {params: {q: searchTerm}});
+        console.log(response);
+    }
+
     render(){
         return(
-            <h1>Youtube clone</h1>        )
+            <Grid justify="center" container spacing={10}>
+                <Grid item xs={11}>
+                    <Grid container spacing ={10}>
+                        <Grid item xs={12}>
+                            <SearchBar onFormSubmit={this.handleSubmit}/>
+                        </Grid>
+                        <Grid item xs ={8}>
+                            <VideoDetail />
+                        </Grid>
+                        <Grid item xs ={4}>
+                            {/***Video List */}
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+        )
     }
 
 }
